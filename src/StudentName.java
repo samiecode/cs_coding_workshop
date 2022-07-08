15a1c8605879374e7984cc6579fc695bfec80185
Azubike Samie <codewithsamie@gmail.com>
2022-07-08 18:44:10 +0100

    Student Name Class

--- src/StudentName.java
+++ src/StudentName.java
@@ -7,23 +7,23 @@ public class StudentName {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        String[] names = new String[10];
+        String[] studentName = new String[10];
 
         System.out.println("Enter 10 Student Names");
-        for (int i = 0; i < names.length; i++) {
+        for (int i = 0; i < studentName.length; i++) {
             System.out.print((i+1) + " Student Name: ");
-            names[i] = input.nextLine();
+            studentName[i] = input.nextLine();
         }
-        
+
         System.out.println("Using for loop to display the 10 student name: ");
-        for (int i = 0; i < names.length; i++) {
-            System.out.print(names[i] + ", ");
+        for (int i = 0; i < studentName.length; i++) {
+            System.out.print(studentName[i] + ", ");
         }
         System.out.println(); // blank line.
 
         System.out.println("Using enhanced for loop to display the 10 student name: ");
-        for(String item : names){
-            System.out.print(item + ", ");
+        for(String eachStudent : studentName){
+            System.out.print(eachStudent + ", ");
         }
 
     }
